1639f3c729b4789cccc2a5da857d85ac7623ea9b
Nub4Ever <rbocanu@yahoo.com>
2023-07-31 14:54:59 +0100

    made springboot challenge work

--- java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
@@ -1,23 +1,51 @@
 package com.db.grad.javaapi.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dogs")
 public class Dog
 {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long dog_id;
+
     private String name;
+    private int age;
 
+    @Column(name = "dog_id")
     public long getId() {
-        return id;
+        return dog_id;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setId(long dog_id) {
+        this.dog_id = dog_id;
     }
 
+    @Column(name = "name")
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Column(name = "age")
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "dog_id=" + dog_id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
