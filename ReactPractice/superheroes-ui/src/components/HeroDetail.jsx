f9a024ec26a52eff739db15de79234b0db567cfb
Nub4Ever <rbocanu@yahoo.com>
2023-07-31 16:32:27 +0100

    react practice

---
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import batman from '../images/batman.jpg'
+
+const HeroDetail = (props) => {
+  return (
+    <div>
+        <img src={batman} alt="Batman" height="250px"/>
+        <p id="blue-alias">Alias: {props.info.alias}</p>
+        <p class="green-class">Name: {props.info.name}</p>
+        <button class="green-class">Like</button>
+    </div>
+  )
+}
+
+export default HeroDetail
